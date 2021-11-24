3fba298d5c013e3ead9281e576f9ff3aed11865c
nerooren <nerooren4001@gmail.com>
2021-11-24 22:09:04 +0300

    modified brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,35 +5,34 @@ import sys
 
 yes_answer = 'yes'
 no_answer = 'no'
-number1 = random.randint(1, 20) # генерация случайного числа 1
-number2 = random.randint(1, 20) # генерация случайного числа 2
-number3 = random.randint(1, 20) # генерация случайного числа 3
+number1 = random.randint(1, 20)  # генерация случайного числа 1
+number2 = random.randint(1, 20)  # генерация случайного числа 2
+number3 = random.randint(1, 20)  # генерация случайного числа 3
 
 
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ', name + '!')
-    print ('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     
     def even_or_odd():
         question1()
-        if number1 % 2 == 0: # проверка если случайное число 1 делится на 2 без остатка
+        if number1 % 2 == 0:
             yes_answer_check()
         else:
             no_answer_check()
         question2()
-        if number2 % 2 == 0: # проверка если случайное число 2 делится на 2 без остатка
+        if number2 % 2 == 0:
             yes_answer_check()
         else:
             no_answer_check()
         question3()
-        if number3 % 2 == 0: # проверка если случайное число 3 делится на 2 без остатка
+        if number3 % 2 == 0:
             yes_answer_check()
         else:
             no_answer_check()
-        print('Congratulations, ', name + '!')    
-
+        print('Congratulations, ', name + '!')
 
     # question defs
     def question1():
@@ -51,7 +50,7 @@ def main():
     # answer_defs
     def yes_answer_check():
         answer = prompt.string('Your answer: ')
-        if answer.lower() == yes_answer: # проверка если пользователь ответил yes
+        if answer.lower() == yes_answer:  # если пользователь ответил yes
                 print('Correct!')
         else: 
                 print(f'{no_answer} is wrong answer ;(. Correct answer was {yes_answer}.')
@@ -61,7 +60,7 @@ def main():
 
     def no_answer_check():
         answer = prompt.string('Your answer: ')
-        if answer.lower() == no_answer: # проверка если пользователь ответил no
+        if answer.lower() == no_answer:  # если пользователь ответил no
                 print('Correct!')
         else:
                 print(f'{yes_answer} is wrong answer ;(. Correct answer was {no_answer}.')
