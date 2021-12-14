f2adbb8cb3339b0270eb940f0a813f9a7e90496c
mbgoodguy <nerooren4001@gmail.com>
2021-12-15 00:41:13 +0300

    переписал код. функции вызываются в main

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,68 +3,65 @@ import prompt
 import random
 import sys
 
-yes_answer = 'yes'
-no_answer = 'no'
-number1 = random.randint(1, 20)  # генерация случайного числа 1
-number2 = random.randint(1, 20)  # генерация случайного числа 2
-number3 = random.randint(1, 20)  # генерация случайного числа 3
 
-
-def main():  # noqa: C901
+def welcome_user():
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, ', name + '!')
+
+def ask_name():
+    name = input('May I have your name? ')
+    return name
+
+def greet_user(name):
+    print('Hello,', name + '!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
-    # answer_defs
-    def yes_answer_check():
-        answer = prompt.string('Your answer: ')
-        if answer.lower() == yes_answer:  # если пользователь ответил yes
-            print('Correct!')
-        else:
-            print('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
-            print('Let''s try again, ', name + '!')
-            sys.exit()
+def generate_random_num():
+    return random.randint(1, 20)
 
-    def no_answer_check():
-        answer = prompt.string('Your answer: ')
-        if answer.lower() == no_answer:  # если пользователь ответил no
-            print('Correct!')
-        else:
-            print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
-            print('Let''s try again, ', name + '!')
-            sys.exit()
+def question(num):
+    print('Question: ', num)
 
-    # question defs
-    def question1(number1):
-        print('Question: ', number1)
-        if number1 % 2 == 0:
-            yes_answer_check()
-        else:
-            no_answer_check()
+def get_answer():
+    answer = input('Your answer: ')
+    return answer
 
-    def question2(number2):
-        print('Question: ', number2)
-        if number2 % 2 == 0:
-            yes_answer_check()
-        else:
-            no_answer_check()
-
-    def question3(number3):
-        print('Question: ', number3)
-        if number3 % 2 == 0:
-            yes_answer_check()
-        else:
-            no_answer_check()
-
-    def even_or_odd():
-        question1(number1)
-        question2(number2)
-        question3(number3)
-        print('Congratulations, ', name + '!')
-
-    even_or_odd()
+def congratulate_user(name):
+    print(f'Congratulations, {name}!')
     sys.exit()
+
+# answer_defs
+def yes_answer_check(answer, name):
+    if answer.lower() == answer:  # если пользователь ответил yes
+        print('Correct!')
+    else:
+        print('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
+        print('Let''s try again, ', name + '!')
+        sys.exit()
+
+def no_answer_check(answer, name):
+    if answer.lower() == answer:  # если пользователь ответил no
+        print('Correct!')
+    else:
+        print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
+        print('Let''s try again, ', name + '!')
+        sys.exit()
+
+
+def main():
+    welcome_user()
+    user_name = ask_name()
+    greet_user(user_name)
+    i = 1
+    while i <= 3:
+        random_number = generate_random_num()
+        question(random_number)
+        user_answer = get_answer()
+        if random_number % 2 == 0:
+            yes_answer_check(user_answer, user_name)
+        else:
+            no_answer_check(user_answer, user_name)
+        i += 1
+    congratulate_user(user_name)
 
 
 main()
