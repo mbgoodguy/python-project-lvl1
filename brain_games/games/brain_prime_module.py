d1450abb8e319f89cb972483d0086dd1b0cab33d
mbgoodguy <nerooren4001@gmail.com>
2021-12-24 02:45:49 +0300

    modified brain_prime_module

--- brain_games/games/brain_prime_module.py
+++ brain_games/games/brain_prime_module.py
@@ -2,14 +2,6 @@ import random
 TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def generate_round():
-    num = random.randint(2, 30)
-    if is_prime(num) is True:
-        return str(num), 'yes'  # возврат числа (question) и ответа (answer), которые будут использованы в engine.py   # noqa: E501
-    else:
-        return str(num), 'no'
-
-
 def is_prime(num):
     for i in range(2, num // 2 + 1):
         if (num % i == 0):
@@ -17,3 +9,11 @@ def is_prime(num):
             return False
         # print(" # Число не является простым")
         return True
+
+
+def generate_round():
+    num = random.randint(2, 30)
+    if is_prime(num) is True:
+        return str(num), 'yes'  # возврат числа (question) и ответа (answer), которые будут использованы в engine.py   # noqa: E501
+    else:
+        return str(num), 'no'
