9a189a262c6f4455ca359564992aaaa288d691df
mbgoodguy <87648053+mbgoodguy@users.noreply.github.com>
2021-12-24 02:05:55 +0300

    Update and rename brain_calc.py to brain_calc_module.py

---
+++ brain_games/games/brain_calc_module.py
@@ -0,0 +1,21 @@
+import random
+TASK = "What is the result of the expression?"
+
+
+def answer_check(random_number1, random_operation, random_number2):
+    if random_operation == '+':
+        return random_number1 + random_number2
+    if random_operation == '-':
+        return random_number1 - random_number2
+    if random_operation == '*':
+        return random_number1 * random_number2
+
+
+def generate_round():
+    operations = ['+', '-', '*']
+    random_number1 = random.randint(1, 20)
+    random_number2 = random.randint(1, 20)
+    random_operation = random.choice(operations)
+    question = f"{random_number1} {random_operation} {random_number2}"
+    answer = answer_check(random_number1, random_operation, random_number2)  # noqa: E501
+    return question, str(answer)
