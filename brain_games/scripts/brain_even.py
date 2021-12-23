cd738ec5ad6c0807f9736c679fe2b10bc01f60f9
mbgoodguy <87648053+mbgoodguy@users.noreply.github.com>
2021-12-24 02:10:49 +0300

    Create brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from brain_games.engine import start_game
+from brain_games.games import brain_even_module
+
+
+def main():
+    start_game(brain_even_module)  # аргумент - импортированный соответствующий модуль игры  # noqa: E501
+
+
+if __name__ == '__ main__':
+    main()
