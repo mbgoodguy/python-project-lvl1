d7f6d28b3d5e70c0ba6d18685605b473d63d2c90
mbgoodguy <87648053+mbgoodguy@users.noreply.github.com>
2021-12-28 08:46:40 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -4,7 +4,7 @@ from brain_games.games import progression
 
 
 def main():
-    start_game(progression)  # аргумент - импортированный соответствующий модуль игры  # noqa: E501
+    start_game(progression)
 
 
 if __name__ == '__main__':
